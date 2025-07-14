8f9946171c33c238ee8e26d3a620aa6f0ee83305
irensula <irinawebdev@gmail.com>
2025-07-14 10:29:07 +0300

    gaps_task: gaps

--- opetuspeli-app/frontend/components/Sentence.js
+++ opetuspeli-app/frontend/components/Sentence.js
@@ -1,21 +1,23 @@
 import { View, Text } from 'react-native';
 
 const Sentence = ({ sentence, word, selectedLanguage }) => {
-    
+    console.log('Word', word);
     function escapeRegExp(string) {
         return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     }
 
 
     const fullSentence = sentence[`sentence_${selectedLanguage}`];
-    const gapdWord = word[`value_${selectedLanguage}`];
+    const gapdWord = word?.[`value_${selectedLanguage}`] || '';
     const escapedWord = escapeRegExp(gapdWord);
-    const sentenceWithGap = fullSentence.replace(new RegExp(`\\b${escapedWord}\\b`, 'i'), '_____');
-    
+    const sentenceWithGap = fullSentence.replace(new RegExp(`\\b${escapedWord}(?:'s)?\\b`, 'i'),
+        '_____');
+    console.log('Full sentence:', fullSentence);
+    console.log('Sentence with gap', sentenceWithGap);
     return (
         <View>
-            <Text>{sentenceWithGap[`sentence_${selectedLanguage}`]}</Text>
-            <Text>{fullSentence}</Text>
+            <Text>{sentenceWithGap}</Text>
+            {/* <Text>{fullSentence}</Text> */}
         </View>
     )
 }
